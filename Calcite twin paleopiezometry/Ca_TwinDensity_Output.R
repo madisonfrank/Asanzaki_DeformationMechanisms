5dee0b20d4df11beddcb38af4ad98fb803692637
Madison Frank <33042916+madisonfrank@users.noreply.github.com>
2023-11-28 13:27:47 +0900

    Update Ca_TwinDensity_Output.R
    Combine reading in file, calculating diff stress, and plotting into one file

--- Calcite twin paleopiezometry/Ca_TwinDensity_Output.R
+++ Calcite twin paleopiezometry/Ca_TwinDensity_Output.R
@@ -1,30 +1,37 @@
 ## Script to calculate stress from calcite twin density following Ryback et al., 2013 paleopiezometer
-## Created by Madison Frank, updated 2023/05/08
+## Created by Madison Frank, updated 2023/11/28
 
-# load in functions
-source("Ca_Twinning_Functions.R")
-
+library(openxlsx)
 library(plotly)
 library(ggplot2)
 
 ### Load raw data ### -------------------------------------------------------------------------
 
 # Read in raw twin count data and calculate 
-twin_data <- getRawTwins(filepath = "Ca_TwinDensity.xlsx", 
-                         sheet = "Ca_TwinDensity", startRow = 2, 
-                         colnames = c('Thin.Section', 'Shear.localisation', 'Distance.(um)', 'Twin.Count'))
+ca_twin_raw <- read.xlsx(filepath = "BLM_Ca_TwinDensity.xlsx", sheet = "Ca_Twins", startRow = 2, colnames = T)
 
-# change sample name
-twin_data$name <- gsub(pattern = "ESZL-1", replacement = "SZLE-1", x = twin_data$name)
-
-
-### Boxplots to visualise outliers ###---------------------------------------------------------
-
-fig <- plot_ly(data = twin_data, x = ~name, y = ~density, type = "box", color = ~localisation, hoverinfo = 'y') %>%
-  layout(title = 'Twin densities', 
-         yaxis = list(title = 'Twin density (# of twins/mm)'))
-fig
-
+### Calculate differential stress ###----------------------------------------------------------
+ 
+# Create a data frame holding the number of twins measured over a given distance. Only keep rows with data (complete cases)
+twin_data <- ca_twin_raw[,c('twin_count', 'distance_um')]
+twin_data <- twin_data[complete.cases(twin_data),]
+  
+colnames(twin_data) <- c("twins", "distance_um")
+str(twin_data)
+  
+# Convert um to mm
+twin_data$distance_mm <- twin_data$distance_um/1000
+  
+# Calculate twins/mm
+twin_data$density <- twin_data$twins/twin_data$distance_mm
+  
+# Calculate differential stress following Rybacki et al., 2013 equation 7
+twin_data$diffStress <- 19.5 * sqrt(twin_data$density)
+  
+twin_data$error_high <- (19.5 + 9.8) * sqrt(twin_data$density)
+twin_data$error_low <- (19.5 - 9.8) * sqrt(twin_data$density)
+  
+### Generate plot ### -------------------------------------------------------------------------
 # plot with ggplot for exporting
 ggRyb <- ggplot(data = twin_data)+
   geom_point(aes(x = density, y = diffStress), size = 2)+
